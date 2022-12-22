fee742be668122c1049896eabd9c0b47ce9ef381
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-12-22 22:20:54 +0100

    feat: define user respository

---
+++ internal/core/ports/repositories.go
@@ -0,0 +1,8 @@
+package ports
+
+import "github.com/tkanzakic/cellar/internal/core/domain"
+
+type UserRepository interface {
+	Get(id string) (*domain.User, error)
+	Create(user *domain.User) (*domain.User, error)
+}
